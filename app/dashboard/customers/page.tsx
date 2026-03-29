c68f6cb1eb3357632cf0c7fd80ce317e906823dd
Muhammad Ali <alisdigitalwork@gmail.com>
2026-03-29 19:48:21 +0500

    Ch 15 completed => (authentication, metadata, authorization)

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,7 @@
+import { Metadata } from "next";
+export const metadata: Metadata = {
+    title: "Customers"
+}
 const Page = () => {
     return ( <p>Customers Page</p> );
 }
--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,8 +1,26 @@
 "use server";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
 import { z } from "zod";
+// Authenticate
+export async function authenticate(prevState: string | undefined, formData: FormData){
+  try {
+    await signIn('credentials', formData);
+  } catch (err) {
+    if(err instanceof AuthError){
+      switch(err.type){
+        case 'CredentialsSignin':
+          return 'Invalid credentials'
+        default:
+          return "Something went wrong"
+      }
+    }
+    throw err;
+  }
+}
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 const FormSchema = z.object({
   id: z.string(),
